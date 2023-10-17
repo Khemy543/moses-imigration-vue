6b2acb9122ef1bc11eace8026dcd3955780fa812
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2023-10-17 07:15:45 +0000

    made changes

---
+++ src/components/About/HeroSection.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="parallax family-header" data-parallax-background-ratio="0.5">
+        <div class="opacity-extra-medium bg-extra-dark-gray"></div>
+        <div class="container">
+            <div class="row align-items-stretch justify-content-center small-screen">
+                <div
+                class="col-12 position-relative page-title-extra-small text-center d-flex align-items-center justify-content-center flex-column"
+                >
+                <h1 class="alt-font text-white opacity-6 margin-20px-bottom">
+                    About
+                </h1>
+                <h3
+                    class="text-white alt-font font-weight-500 w-55 md-w-65 sm-w-80 center-col xs-w-100 letter-spacing-minus-1px line-height-50 sm-line-height-45 xs-line-height-30 no-margin-bottom"
+                >
+                    <!-- Beware Of Fraud -->
+                    {{ 'put title here' }}
+                </h3>
+                </div>
+                <div class="down-section text-center">
+                <a href="#about" class="section-link">
+                    <vue-feather
+                    type="arrow-down"
+                    stroke="#bf8c4c"
+                    size="24"
+                    ></vue-feather>
+                </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
